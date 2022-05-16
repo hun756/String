d239b6c062f1b2e54a6e2b0ee55a38fec27aaf10
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2022-05-16 10:34:25 +0300

    Refactoring
      -  The include guard has been combined with a randomized string of 
         characters to avoid conflicts with other string classes.
      -  The tag, which is the character control structure, has been moved 
         within the namespace.
      -  Typedef statements have been written to make unsigned and long 
         types shorter.
      -  The snake case method used in naming the iterator classes was 
         removed and the camel case naming method was used.
      -  Unnecessary white spaces at the end of comments have been 
         cleaned.
      -  Instead of naming String, naming StringBase, which is the base class, 
         is used. Classes that differ from the character type will be generated 
         from the base class as a template.

--- include/string.hpp
+++ include/string.hpp
@@ -1,39 +1,44 @@
-#ifndef STRING_HPP
-#define STRING_HPP
+#ifndef STRING_HPP_wt709n
+#define STRING_HPP_wt709n
 
 #include <type_traits>
 #include <memory>
 #include <cstring>
 
 #define DEFAULT_CAP 15
+#define CPP_2020_OR_HIGHER 202002L
 
 #ifdef ENABLE_COMPABILITY_W_STD_STRING
 #include <string>
 #endif
 
-template <class T>
-struct tag_t
-{
-};
-template <typename T>
-constexpr tag_t<T> tag{};
 namespace kor
 {
+    template <class T>
+    struct tag_t
+    {
+    };
+
+    template <typename T>
+    constexpr tag_t<T> tag{};
+
     template <typename T, typename... Ts>
     constexpr bool _is_char_(tag_t<T>, Ts &&...) { return false; }
     constexpr bool _is_char_(tag_t<char>) { return true; }
 
-#if __cplusplus >= 202002L
+#if __cplusplus >= CPP_2020_OR_HIGHER
     constexpr bool _is_char_(tag_t<char_8t>)
     {
         return true;
     }
+
 #endif
 
     constexpr bool _is_char_(tag_t<char16_t>)
     {
         return true;
     }
+
     constexpr bool _is_char_(tag_t<char32_t>) { return true; }
     constexpr bool _is_char_(tag_t<wchar_t>) { return true; }
 
@@ -43,26 +48,54 @@ namespace kor
 
 namespace kor
 {
+    typedef unsigned __u;             ///< unsigned (int)
+    typedef unsigned short __us;      ///< unsigned (int16_t)
+    typedef unsigned long __ul;       ///< unsigned long
+    typedef unsigned long long __ull; ///< unsigned long long
+    typedef long long __ll;           ///< long long
+    typedef long double __ld;         ///< long double
+}
+
+namespace kor
+{
+    class ConstIterator
+    {
+    };
+    class Iterator : public ConstIterator
+    {
+    };
+}
+
+namespace kor
+{
     template <typename charType>
-    class String
+    class StringBase
     {
         static_assert(kor::is_char<charType>, "String class works only with char");
+
+        using reference = charType &;
+        using ptr_type = charType *;
+        using iterator = kor::Iterator;
+        using const_iterator = kor::ConstIterator;
+        using difference_type = ptrdiff_t;
 
     public:
         static constexpr size_t npos = -1;
 
         ///< Life Time Management
 
+#if __GNUC__ || __MINGW32__ || __MINGW64__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wuninitialized"
+#endif
         /**
          * @brief Construct a new String object
          *
          */
-        String() : _capacity(DEFAULT_CAP), _size{0}
+        StringBase() : _capacity(DEFAULT_CAP), _size{0}
         {
             _data = std::make_unique<charType[]>(_capacity + 1);
-            _data[0] = '\0';
+            _data[0] = 0x00;
         }
 
         /**
@@ -71,8 +104,20 @@ namespace kor
          *
          * @param nptr
          */
-        String(std::nullptr_t n)
+        StringBase(std::nullptr_t n)
         {
+        }
+
+        /**
+         * @brief Construct a new String object
+         *
+         * @param _cap
+         */
+        StringBase(__u _cap)
+            : _size(0), _capacity(_cap)
+        {
+            _data = std::make_unique<charType[]>(_capacity + 1);
+            _data[0] = 0x00;
         }
 
         /**
@@ -80,41 +125,42 @@ namespace kor
          *
          * @param cStr
          */
-        String(const char *cStr)
-            :  _size(std::strlen(cStr))
-               
+        StringBase(const charType *cStr)
+            : _size(std::strlen(cStr))
+
         {
             _capacity = _size <= 15 ? DEFAULT_CAP : _size;
-            _data= std::make_unique<charType[]>(_capacity + 1);
+            _data = std::make_unique<charType[]>(_capacity + 1);
             std::memcpy(_data.get(), cStr, _size + 1);
         }
 
         /**
          * @brief Construct a new String object
-         * 
-         * @param other 
+         *
+         * @param other
          */
-        String(const String &other)
+        StringBase(const StringBase &other)
         {
             _capacity = other._capacity;
             _size = other._size;
             _data = std::make_unique<char[]>(_capacity + 1);
-            std::memcpy(_data.get(), other._data.get(), other._size + 1); 
+            std::memcpy(_data.get(), other._data.get(), other._size + 1);
         }
 
         /**
          * @brief Construct a new String object
-         * 
-         * @param other 
+         *
+         * @param other
          */
-        String(String &&other) noexcept
-            :  _capacity{other._capacity}, _size{other._size}
+        StringBase(StringBase &&other) noexcept
+            : _capacity{other._capacity}, _size{other._size}
         {
-           _data = std::move(other._data);
+            _data = std::move(other._data);
         }
 
+#if __GNUC__ || __MINGW32__ || __MINGW64__
 #pragma GCC diagnostic pop
-
+#endif
         /**
          * @brief
          *
@@ -134,39 +180,41 @@ namespace kor
         charType *c_str() const { return _data.get(); }
 
         /**
-         * @brief 
-         * 
-         * @return size_t 
+         * @brief
+         *
+         * @return size_t
          */
         size_t capacity() const { return _capacity; }
 
         /**
-         * @brief 
-         * 
-         * @return size_t 
+         * @brief
+         *
+         * @return size_t
          */
         size_t size() const { return _size; }
 
         /**
-         * @brief 
-         * 
-         * @return size_t 
+         * @brief
+         *
+         * @return size_t
          */
         size_t length() const { return _size; }
 
         /**
-         * @brief 
-         * 
-         * @return size_t 
+         * @brief
+         *
+         * @return size_t
          */
         size_t empty() const { return _size == 0; }
 
-    private:
     private:
         std::unique_ptr<charType[]> _data;
         size_t _capacity;
         size_t _size;
     };
+
+    using String = StringBase<char>;
+    using WString = StringBase<wchar_t>;
 }
 
-#endif
+#endif /** End of include guard : STRING_HPP_wt709n */
